718efdc50ec68ef1e775d8b6b3f73b57955d5d91
haitam <10712736+htmlrb@users.noreply.github.com>
2019-07-28 23:49:45 -0700

    debug

--- src/main/scala/beam/agentsim/agents/ridehail/allocation/PoolingAlonsoMora.scala
+++ src/main/scala/beam/agentsim/agents/ridehail/allocation/PoolingAlonsoMora.scala
@@ -208,6 +208,14 @@ class PoolingAlonsoMora(val rideHailManager: RideHailManager)
                 removeRequestFromBuffer(customerIdToReqs(orig.person.get.personId))
               }
               if (rideHailManager.beamServices.geo.distUTMInMeters(orig.activity.getCoord, dest.activity.getCoord) < rideHailManager.beamServices.beamConfig.beam.agentsim.thresholdForWalkingInMeters) {
+                val coord = newRideHailRequest.get.pickUpLocationUTM
+                skimmer.countEvents(
+                  tick,
+                  rideHailManager.beamServices.beamScenario.tazTreeMap.getTAZ(coord.getX, coord.getY).tazId,
+                  Id.create("pooling-alonso-mora", classOf[VehicleManager]),
+                  "toWalk-customer-"+newRideHailRequest.get.customer.personId,
+                  count = 1
+                )
                 scheduleToCache = scheduleToCache :+ orig
                 None
               } else {
